025cdf7f0de5fdfd98b6007dfb6d0a8764ea50da
Chi Man Lei <chi.lei@laxino.com>
2016-07-08 18:35:12 +0800

    Refactored method #default_templates_path in a DRY way

--- lib/luban/deployment/packages/nginx/base.rb
+++ lib/luban/deployment/packages/nginx/base.rb
@@ -10,7 +10,7 @@ module Luban
         end
 
         def default_templates_path
-          @default_templates_path ||= Pathname.new(__FILE__).dirname.join('templates').realpath
+          @default_templates_path ||= super(__FILE__)
         end
 
         %i(config_test quit_process reload_process reopen_logs).each do |action|
